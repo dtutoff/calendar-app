feb8a713e3c88ab724e6d23358dae7d77e0caa53
Denis Tutov <denistutov@hotmail.com>
2026-06-05 09:30:14 +0300

    feat: complete practice 2-10 with cmd cli

---
+++ module-2/practice-5/cmd/cmd.go
@@ -0,0 +1,130 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/c-bata/go-prompt"
+	"github.com/dtutoff/app/calendar"
+	"github.com/dtutoff/app/events"
+	"github.com/google/shlex"
+)
+
+type Cmd struct {
+	calendar *calendar.Calendar
+}
+
+func NewCmd(c *calendar.Calendar) *Cmd {
+	return &Cmd{calendar: c}
+}
+
+func (c *Cmd) executor(input string) {
+	parts, err := shlex.Split(input)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	cmd := strings.ToLower(parts[0])
+
+	switch cmd {
+	case "add":
+		if len(parts) < 4 {
+			fmt.Println("invalid input")
+			fmt.Println(`Syntax: add "id" "name event" "date" "priority"`)
+			return
+		}
+
+		title := parts[1]
+		date := parts[2]
+		priority := events.Priority(parts[3])
+
+		e, err := c.calendar.AddEvent(title, date, priority)
+		if err != nil {
+			fmt.Println("Error adding event:", err)
+		} else {
+			fmt.Println("Event:", e.Title, "added")
+		}
+	case "list":
+		c.calendar.ShowEvents()
+
+	case "remove":
+		err := c.calendar.DeleteEvent(parts[1])
+
+		eventId := parts[1]
+
+		if err != nil {
+			fmt.Println("Error deleting event:", err)
+		} else {
+			fmt.Println("Event with ID:", eventId, "was deleted")
+		}
+
+	case "update":
+
+		if len(parts) < 5 {
+			fmt.Println("invalid input")
+			fmt.Println(`Syntax: update "id" "name event" "date" "priority"`)
+			return
+		}
+
+		id := parts[1]
+		title := parts[2]
+		date := parts[3]
+		priority := events.Priority(parts[4])
+
+		err := c.calendar.EditEvent(id, title, date, priority)
+		if err != nil {
+			fmt.Println("Error updating event:", err)
+		}
+
+	case "exit":
+		err := c.calendar.Save()
+		if err != nil {
+			fmt.Println("Saving error:", err)
+		}
+		os.Exit(0)
+
+	case "help":
+		commandList := map[string]string{
+			"add":    "Add event - Syntax: add \"id\" \"name event\" \"date\" \"priority\"",
+			"list":   "Show all events - Syntax: list",
+			"remove": "Delete event - Syntax: remove \"id\"",
+			"update": "Update event - Syntax: update \"id\" \"name event\" \"date\" \"priority\"",
+			"help":   "Show list of commands - Syntax: help",
+			"exit":   "Exit program - Syntax: exit",
+		}
+
+		fmt.Println("\nAvailable commands:")
+		fmt.Println("-------------------")
+		for command, desc := range commandList {
+			fmt.Printf("  %-8s - %s\n", command, desc)
+		}
+
+	default:
+		fmt.Println("Unknown command")
+		fmt.Println("Enter help for list of commands")
+	}
+}
+
+func (c *Cmd) completer(d prompt.Document) []prompt.Suggest {
+	suggestions := []prompt.Suggest{
+		{Text: "add", Description: "Edd event"},
+		{Text: "list", Description: "Show events"},
+		{Text: "remove", Description: "Delete event"},
+		{Text: "update", Description: "Update event"},
+		{Text: "help", Description: "Show commands"},
+		{Text: "exit", Description: "Exit program"},
+	}
+
+	return prompt.FilterHasPrefix(suggestions, d.GetWordBeforeCursor(), true)
+}
+
+func (c *Cmd) Run() {
+	p := prompt.New(
+		c.executor,
+		c.completer,
+		prompt.OptionPrefix("> "),
+	)
+	p.Run()
+}
--- module-2/practice-5/main.go
+++ module-2/practice-5/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/dtutoff/app/calendar"
+	"github.com/dtutoff/app/cmd"
 	"github.com/dtutoff/app/storage"
 )
 
@@ -14,41 +15,12 @@ func main() {
 	}
 	c := calendar.NewCalendar(s)
 
-	z := storage.NewZipStorage("./calendar.zip")
-	c1 := calendar.NewCalendar(z)
-	err5 := c1.Save()
-	if err5 != nil {
-		return
-	}
-
 	err := c.Load()
 	if err != nil {
 		fmt.Println("Loading error:", err)
 		return
 	}
 
-	event1, err1 := c.AddEvent("Meeting will be here!!!!!", "2025/06/12", "high")
-	if err1 != nil {
-		fmt.Println("Error:", err1)
-		return
-	} else {
-		err := c.SetEventReminder(event1.ID, event1.Title, "2025/07/12")
-		if err != nil {
-			fmt.Println("Error:", err)
-		}
-		fmt.Println(event1.Title, "added")
-	}
-
-	if event1.Reminder != nil {
-		event1.Reminder.Send()
-	} else {
-		fmt.Println("No Reminder")
-	}
-
-	err3 := c.Save()
-	if err3 != nil {
-		return
-	}
-
-	c.ShowEvents()
+	cli := cmd.NewCmd(c)
+	cli.Run()
 }
